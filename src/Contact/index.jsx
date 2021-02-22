a9f5e0c52767b69ba4e42be64e536b9d615bd1bb
leovenom <leonardt@oputlook.com>
2021-02-22 11:41:58 +0100

    email aktive

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -1,13 +1,14 @@
 import emailjs from 'emailjs-com';
 import React from 'react';
-import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormLabel, FormButton, FormInputText } from './ContactElements'
+import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormLabel, FormButton, FormInputText } from './ContactElements';
+
 
 const SignIn = () => {
 
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_3t552xh', 'template_pxi9lbh', e.target, 'user_12T6PVF5oIb4Wm6EGuEhu')
+    emailjs.sendForm('service_3t552xh', 'template_aky6j3h', e.target, 'user_12T6PVF5oIb4Wm6EGuEhu')
     .then((result) => {
       alert('Vielen Dank, ihre Mail wurde erfolgreich versendet!', result.status, result.text);
   }, (error) => {
@@ -25,7 +26,7 @@ const SignIn = () => {
             <Form onSubmit={sendEmail}>
               <FormH1>Ich freue mich über Ihre Nachricht</FormH1>
               <FormLabel htmlFor='for'>Name:</FormLabel>
-                <FormInput htmlFor='nome' name='user_nome' placeholder="Ihr Name" required />
+                <FormInput htmlFor='nome' type="text" name='user_nome' placeholder="Ihr Name" required />
               <FormLabel htmlFor='for'>E-Mail:</FormLabel>
                 <FormInput htmlFor='email' type='email' name='user_email' placeholder="kontakt@email.com" required />
               <FormLabel htmlFor='for'>Nachricht:</FormLabel>
